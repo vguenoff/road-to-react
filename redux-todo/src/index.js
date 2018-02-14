7ccf5c87f307eb36af58038b689db9c285e17ad1
Vasil Genov <vasil.genov@ad.mentormate.bg>
2018-02-14 17:08:29 +0200

    redux todo

---
+++ redux-todo/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+
+import './index.css';
+
+const TodoApp = () => <div>Todo App</div>;
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
