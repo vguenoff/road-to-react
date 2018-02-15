1d41db1962c954e65227935374e73a1441a7c289
Vasil Genov <vasil.genov@ad.mentormate.bg>
2018-02-15 12:52:18 +0200

    connected components and middleware

---
+++ redux-todo/src/TodoApp.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TodoApp = () => <p>TodoApp</p>;
+
+export default TodoApp;
--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { createLogger } from 'redux-logger';
 
 import './index.css';
 
-// action types
 const actions = {
   addTodo: 'ADD_TODO',
   toggleTodo: 'TOGGLE_TODO',
-  filterSet: 'FILTER_SET',
+  // filterSet: 'FILTER_SET',
 };
 
 const todos = [
@@ -38,41 +39,46 @@ const todoReducer = (state = todos, action) => {
 };
 
 // filter reducer
-const filterReducer = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-    case actions.filterSet:
-      return action.payload;
-    default:
-      return state;
-  }
-};
+// const filterReducer = (state = 'SHOW_ALL', action) => {
+//   switch (action.type) {
+//     case actions.filterSet:
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// };
 
 // action creators
-const doAddTodo = (id, name) => ({
-  type: actions.addTodo,
-  payload: { id, name },
-});
+// const doAddTodo = (id, name) => ({
+//   type: actions.addTodo,
+//   payload: { id, name },
+// });
 
 const doToggleTodo = id => ({
   type: actions.toggleTodo,
   payload: { id },
 });
 
-const doSetFilter = filter => ({
-  type: actions.filterSet,
-  payload: filter,
-});
+// const doSetFilter = filter => ({
+//   type: actions.filterSet,
+//   payload: filter,
+// });
 
 // root reducer
 const rootReducer = combineReducers({
   todoReducer,
-  filterReducer,
+  // filterReducer,
 });
 
+// logger middleware
+const logger = createLogger();
+
 // creating a store
-const store = createStore(rootReducer);
-console.log('-----------------initial state-----------------');
-console.log(store.getState());
+const store = createStore(
+  rootReducer,
+  undefined,
+  applyMiddleware(logger),
+);
 
 // view layer
 const TodoItem = ({ todo, onToggleTodo }) => {
@@ -91,35 +97,30 @@ const TodoItem = ({ todo, onToggleTodo }) => {
   );
 };
 
-const TodoList = ({ todos, onToggleTodo }) => (
+const TodoList = ({ todos }) => (
   <div>
     {
       todos.map(todo =>
-        (<TodoItem
+        (<ConnectedTodoItem
           key={todo.id}
           todo={todo}
-          onToggleTodo={onToggleTodo}
         />))
     }
   </div>
 );
 
-const TodoApp = ({ todos, onToggleTodo }) => (
-  <TodoList
-    todos={todos}
-    onToggleTodo={onToggleTodo}
-  />
+const TodoApp = () => <ConnectedTodoList />;
+
+// connecting react and redux
+const mapStateToProps = state => ({ todos: state.todoReducer });
+const mapDispatchToProps = dispatch => ({ onToggleTodo: id => dispatch(doToggleTodo(id)) });
+
+const ConnectedTodoList = connect(mapStateToProps)(TodoList);
+const ConnectedTodoItem = connect(null, mapDispatchToProps)(TodoItem);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root'),
 );
-
-function render() {
-  ReactDOM.render(
-    <TodoApp
-      todos={store.getState().todoReducer}
-      onToggleTodo={id => store.dispatch(doToggleTodo(id))}
-    />, document.getElementById('root'),
-  );
-}
-
-store.subscribe(render);
-render();
-
